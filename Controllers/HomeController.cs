a6d14643ec8b45737617e3a189a8a66da2e7fd4a
Oskar Kapica <121721977+kapicaoskar@users.noreply.github.com>
2023-04-22 00:49:39 +0200

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenQA.Selenium.Chrome;
+
+namespace WebApplication3.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("chromedriver")]
+        public IActionResult Test()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            using (var driver = new ChromeDriver(chromeOptions))
+            {
+                driver.Navigate().GoToUrl("https://www.google.com");
+            }
+            return Ok();
+        }
+    }
+}
